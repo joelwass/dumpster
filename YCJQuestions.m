45337fc1a5773cf13b6afa54e498e2419f19ce6a
Joel Wasserman <joel.wasserman8@gmail.com>
2014-09-03 08:04:59 -0400

    working on pulling parse id number for questions and answers

--- YCJQuestions.m
+++ YCJQuestions.m
@@ -7,11 +7,25 @@
 //
 
 #import "YCJQuestions.h"
+#import <Parse/Parse.h>
 
 @implementation YCJQuestions
+id questionNumber = 0;
+NSString *question = NULL;
 
 - (void)buildQuestions:(NSMutableDictionary *)questionList
 {
+    questionNumber = arc4random() % 100;
     
+    PFQuery *query = [PFQuery queryWithClassName:@"Questions"];
+    [query whereKey:@"QNumber" equalTo:questionNumber];
+    [query getFirstObjectInBackgroundWithBlock:^(PFObject *object, NSError *error) {
+        if (!object) {
+            // Did not find any UserStats for the current user
+        } else {
+            // Found UserStats
+            int highScore = [[object objectForKey:@"highScore"] intValue];
+        }
+    }];
 }
 @end
