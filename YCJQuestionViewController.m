d870bf7c2de38b22657cff99d724df1579278182
Joel Wasserman <joel.wasserman8@gmail.com>
2014-09-19 18:31:07 -0400

    working update on next question;

--- YCJQuestionViewController.m
+++ YCJQuestionViewController.m
@@ -34,30 +34,16 @@
     return self;
 }
 
-
-- (void)viewDidLoad
+- (void)populateQuestions
 {
-    
-   
-    
-    [super viewDidLoad];
-    
-    
-    
-    
-    /*tried putting the pfquery before super viewdidload, turns out that sometimes the query hasn't loaded
-     yet and the view loads with nothing in the buttons and labels, so let's keept it so the pf query
-     happens after viewdidload.
-    */
-    
     
     PFQuery *answerQuery = [PFQuery queryWithClassName:@"Answers"];
     int random = arc4random_uniform(3);
-  
+    
     NSString *key = [NSString stringWithFormat:@"%d",random];
     self.key = key;
-  
-
+    
+    
     
     PFQuery *questionQuery = [PFQuery queryWithClassName:@"Questions"];
     [questionQuery whereKey:@"QNumber" containsString:key];
@@ -86,19 +72,38 @@
             
             
         }
-    
+        
         
         /*
          need to figure out how to set the correct answer to a certain button and then
          remember which button that one is, somehow keep track of it so we know which button
-         needs to be pressed in order to continue, also figure out how to randomize buttons that 
+         needs to be pressed in order to continue, also figure out how to randomize buttons that
          get the correct answer set to it. well i guess that randomization will be done on the parse end
          of things so we just need to keep track of which button gets the correct answer
          set to it
          */
         
     }];
+    
 
+}
+
+
+- (void)viewDidLoad
+{
+    
+   
+    
+    [super viewDidLoad];
+    
+    [self populateQuestions];
+    
+    
+    /*tried putting the pfquery before super viewdidload, turns out that sometimes the query hasn't loaded
+     yet and the view loads with nothing in the buttons and labels, so let's keept it so the pf query
+     happens after viewdidload.
+    */
+    
     
 
     
@@ -113,6 +118,7 @@
     }
 }
 - (IBAction)nextQuestion:(UIButton *)sender {
+    [self populateQuestions];
 }
 
 -(void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender{
