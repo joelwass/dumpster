d8838bad27f4bc76824361c5ae19540c62ba37db
Joel Wasserman <joel.wasserman8@gmail.com>
2014-09-28 23:08:13 -0400

    almost done with design

--- YCJAPViewController.m
+++ YCJAPViewController.m
@@ -39,6 +39,7 @@
 {
 
     [super viewDidLoad];
+    [self.view setBackgroundColor: [self colorWithHexString:@"68C3A3"]];
     [_answerLabel setFont:[UIFont fontWithName:@"Chalkduster" size:18]];
     [_learnMoreButton.titleLabel setFont:[UIFont fontWithName:@"Chalkduster" size:18]];
     [_nextButton.titleLabel setFont:[UIFont fontWithName:@"Chalkduster" size:18]];
@@ -81,6 +82,42 @@
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
     [[NSURLCache sharedURLCache] removeAllCachedResponses];
+}
+
+-(UIColor*)colorWithHexString:(NSString*)hex
+{
+    NSString *cString = [[hex stringByTrimmingCharactersInSet:[NSCharacterSet whitespaceAndNewlineCharacterSet]] uppercaseString];
+    
+    // String should be 6 or 8 characters
+    if ([cString length] < 6) return [UIColor grayColor];
+    
+    // strip 0X if it appears
+    if ([cString hasPrefix:@"0X"]) cString = [cString substringFromIndex:2];
+    
+    if ([cString length] != 6) return  [UIColor grayColor];
+    
+    // Separate into r, g, b substrings
+    NSRange range;
+    range.location = 0;
+    range.length = 2;
+    NSString *rString = [cString substringWithRange:range];
+    
+    range.location = 2;
+    NSString *gString = [cString substringWithRange:range];
+    
+    range.location = 4;
+    NSString *bString = [cString substringWithRange:range];
+    
+    // Scan values
+    unsigned int r, g, b;
+    [[NSScanner scannerWithString:rString] scanHexInt:&r];
+    [[NSScanner scannerWithString:gString] scanHexInt:&g];
+    [[NSScanner scannerWithString:bString] scanHexInt:&b];
+    
+    return [UIColor colorWithRed:((float) r / 255.0f)
+                           green:((float) g / 255.0f)
+                            blue:((float) b / 255.0f)
+                           alpha:1.0f];
 }
 
 @end
