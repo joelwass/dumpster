566105c69dfe486f213c86e7fc6907f9062a0b4f
Joel Wasserman <joel.wasserman8@gmail.com>
2014-09-30 13:31:13 -0400

    working resizing with iphone 4, 4s and 5 and 5s

--- YCJQuestionViewController.m
+++ YCJQuestionViewController.m
@@ -110,7 +110,7 @@
     _button4.titleLabel.textAlignment = NSTextAlignmentCenter;
     [_button4.titleLabel setFont:[UIFont fontWithName:@"Chalkduster" size:18]];
     [_skipButton.titleLabel setFont:[UIFont fontWithName:@"Chalkduster" size:18]];
-        [_questionLabel setFont:[UIFont fontWithName:@"Chalkduster" size:17
+        [_questionLabel setFont:[UIFont fontWithName:@"Chalkduster" size:18
                                  ]];
     
     self.navigationController.navigationBar.translucent = NO;
