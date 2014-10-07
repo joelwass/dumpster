dd6da0f2bcf4f22aece38e66b96982ba6137689a
Joel Wasserman <joel.wasserman8@gmail.com>
2014-10-07 17:58:04 -0400

    working on uitextfield annoying constraints

--- YCJContributeViewController.m
+++ YCJContributeViewController.m
@@ -11,7 +11,8 @@
 
 @interface YCJContributeViewController ()
 
-@property (weak, nonatomic) IBOutlet UILabel *aboutUsLabel;
+
+@property (weak, nonatomic) IBOutlet UITextView *aboutUsField;
 
 @end
 
@@ -34,8 +35,9 @@
 {
     [super viewDidLoad];
     [self.view setBackgroundColor: [self colorWithHexString:@"68C3A3"]];
-    [_aboutUsLabel setFont:[UIFont fontWithName:@"Chalkduster" size:16]];
-    _aboutUsLabel.text = @"Here at IVΣT we not only believe in open sourcing code, we believe in open sourcing education. We encourage you to participate in the general education of humanity by contributing questions to our network of questions. We hope to provide the platform from which education enthusiasts around the world will dive into the World Wide Web. Please, help us. - IVΣT";
+    [_aboutUsField setFont:[UIFont fontWithName:@"Chalkduster" size:16]];
+    [_aboutUsField setBackgroundColor:[self colorWithHexString:@"68C3A3"]];
+    _aboutUsField.text = @"Here at IVΣT we not only believe in open sourcing code, we believe in open sourcing education. We encourage you to participate in the general education of humanity by contributing questions to our network of questions. We hope to provide the platform from which education enthusiasts around the world will dive into the World Wide Web. Please, help us. - IVΣT";
     
 }
 
